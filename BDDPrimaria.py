5980f0108fbfd7f6cfd1fef9ba93324e3416b42a
Liam-Corrales33 <21150@portalcedi.edu.mx>
2025-12-04 16:19:29 -0600

    Update BDDPrimaria.py

---
+++ BDDPrimaria.py
@@ -0,0 +1,19 @@
+#Show students
+def mostrarAlumnos(): 
+  print("Samuel\nPablo\nRoberta\nSonia\nGerardo\n")
+
+#Show professors
+def mostrarProfesores():
+  print("Estefanía\nGrecia\nBrenda\nFabrice\nJuan\n")
+
+#Show subjects
+def mostrarMaterias():
+  print("Matemáticas\nCiencias\nInglés\nHistoria\nFCE\n")
+
+#Each professor's subject
+def profsub():
+  print("Estefanía: Matemáticas\nGrecia: Ciencias\nBrenda: Inglés\nFabrice: Historia\nJuan: FCE")
+
+#Show each student's grades per subject
+def grades():
+  print("Samuel
